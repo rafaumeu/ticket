120472a99bea63172c1aa1dbbf2b31e83a51b0ac
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-10-28 10:51:42 -0300

    adicionada solução ao atualizar status para fechado

--- src/controllers/tickets/updateStatus.js
+++ src/controllers/tickets/updateStatus.js
@@ -1,6 +1,7 @@
 export function updateStatus({req, res, database}) {
   const {id} = req.params
-  
-  database.update("tickets", id, { status: "closed" })
+  const { solution } = req.body
+    
+  database.update("tickets", id, { status: "closed", solution })
   return res.end()
 }
