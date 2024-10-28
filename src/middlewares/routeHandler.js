c122e9e7adb98eb9bc3a3c3b3dcf492e2642f2cc
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-10-28 06:55:34 -0300

    criação do banco de dados

--- src/middlewares/routeHandler.js
+++ src/middlewares/routeHandler.js
@@ -1,13 +1,16 @@
+import { Database } from "../database/database.js";
 import { routes } from "../routes/index.js";
 
+const database = new Database()
 export function routeHandler(req, res) {
   const route = routes.find((route) => {
     return route.method === req.method && route.path === req.url
   })
 
   if(route) {
-    return route.controller({req, res})
+    return route.controller({req, res, database})
   }
+  
 
   return response.writeHead(404).end()
 }
