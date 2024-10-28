f3b5a6424c14518557969bea0b7e4eae162141d8
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-10-28 06:45:36 -0300

    criando objeto ticket

--- src/controllers/tickets/create.js
+++ src/controllers/tickets/create.js
@@ -1,3 +1,15 @@
+import {randomUUID} from "node:crypto";
 export function create({req, res}) {
-  return res.end("Criado com sucesso!");
+  const { equipment, description, user_name } = req.body;
+
+  const ticket = {
+    id: randomUUID() ,
+    equipment,
+    description,
+    user_name,
+    status: "open",
+    created_at: new Date(),
+    updated_at: new Date(),
+  }
+  return res.end(JSON.stringify(ticket));
 }
